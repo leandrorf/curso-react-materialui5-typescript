f109a614d3fab472fd3b62d46d65c07d4dfbbc79
Leandro Rodrigues Fernandes <leandro.rodrigues.fernandes@live.com>
2026-08-06 22:25:27 -0300

    React, Material UI 5 e Typescript: #12 - Layout base responsivo

--- youtube-curso-react-materialui-typescript/src/pages/dashboard/Dashboard.tsx
+++ youtube-curso-react-materialui-typescript/src/pages/dashboard/Dashboard.tsx
@@ -3,7 +3,9 @@ import { LayoutBaseDePagina } from "../../shared/layouts";
 export const Dashboard = () => {
 
     return (
-        <LayoutBaseDePagina titulo="Página inicial">
+        <LayoutBaseDePagina 
+            titulo="Página inicial" 
+            barraDeFerramentas={<>Barra de ferramentas</>}>
             Testando
         </LayoutBaseDePagina>
     );
--- youtube-curso-react-materialui-typescript/src/shared/layouts/LayoutBaseDePagina.tsx
+++ youtube-curso-react-materialui-typescript/src/shared/layouts/LayoutBaseDePagina.tsx
@@ -3,33 +3,41 @@ import { useDrawerContext } from "../contexts";
 
 interface ILayoutBaseDePaginaProps {
     titulo: string;
+    barraDeFerramentas?: React.ReactNode;
     children?: React.ReactNode;
 }
 
-export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({ children, titulo }) => {
+export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({ children, titulo, barraDeFerramentas }) => {
     const smDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
+    const mdDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
     const theme = useTheme();
     const { toggleDrawerOpen } = useDrawerContext();
 
     return (
         <Box sx={{ height: "100%", display: "flex", flexDirection: "column", gap: 1}}>
-            <Box sx={{ padding: 1, height: theme.spacing(12), display: "flex", alignItems: "center", gap: 1}}>
+            <Box sx={{ padding: 1, display: "flex", alignItems: "center", gap: 1, height: theme.spacing(smDown ? 6 : mdDown ? 8 : 12) }}>
                 {smDown && (
                     <IconButton onClick={toggleDrawerOpen}>
                         <Icon>menu</Icon>
                     </IconButton>
                 )}
                 
-                <Typography variant="h5">
+                <Typography 
+                    variant={smDown ? "h5" : mdDown ? "h4" : "h3"}
+                    sx={{ whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis" }}>
                     {titulo}
                 </Typography>
             </Box>
 
-            <Box>
-                Barra de ferramentas
-            </Box>
+            {barraDeFerramentas &&(
+                <Box>
+                    {barraDeFerramentas}
+                </Box>
+            )}
             
-            {children}
+            <Box sx={{ flex: 1, overflow: "auto"}}>
+                {children}
+            </Box>
         </Box>
     );
 };
