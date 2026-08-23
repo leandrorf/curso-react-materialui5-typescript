2a76ca30d5308e479d69354130537d642d254c88
Leandro Rodrigues Fernandes <leandro.rodrigues.fernandes@live.com>
2026-08-23 17:47:55 -0300

    Como configurar variáveis ambiente no React

--- youtube-curso-react-materialui-typescript/src/shared/environment/index.ts
+++ youtube-curso-react-materialui-typescript/src/shared/environment/index.ts
@@ -1,1 +1,22 @@
-export { };
+export const Environment = {
+
+    /**
+     * Define a quantidade de linhas que serão exibidas na listagem de registros.
+     */
+    LIMITE_DE_LINHAS: 10,
+
+    /**
+     * Define o texto exibido no input de busca.
+     */
+    INPUT_DE_BUSCA: 'Pesquisar...',
+    
+    /**
+     * Define o texto exibido quando a listagem está vazia.
+     */
+    LISTAGEM_VAZIA: 'Nenhum registro encontrado.',
+    
+    /**
+     * Define a URL base da API.
+     */
+    URL_BASE: 'http://localhost:3333',
+}
