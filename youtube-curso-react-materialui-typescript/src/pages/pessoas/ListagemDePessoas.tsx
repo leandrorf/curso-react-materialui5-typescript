7a1b960afc305b9a685b38c1eaaa9db3b3349041
Leandro Rodrigues Fernandes <leandro.rodrigues.fernandes@live.com>
2026-08-26 22:04:54 -0300

    #29 - Adicionando paginação na listagem com React

--- youtube-curso-react-materialui-typescript/src/pages/pessoas/ListagemDePessoas.tsx
+++ youtube-curso-react-materialui-typescript/src/pages/pessoas/ListagemDePessoas.tsx
@@ -4,7 +4,7 @@ import { LayoutBaseDePagina } from "../../shared/layouts";
 import { useEffect, useMemo, useState } from "react";
 import { IListagemPessoa, PessoasService } from "../../shared/services/api/pessoas/PessoasService";
 import { useDebounce } from "../../shared/hooks";
-import { LinearProgress, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow } from "@mui/material";
+import { Box, LinearProgress, Pagination, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow } from "@mui/material";
 import { Environment } from "../../shared/environment";
 
 export const ListagemDePessoas: React.FC = () => {
@@ -20,12 +20,16 @@ export const ListagemDePessoas: React.FC = () => {
         return searchParams.get('busca') || '';
     }, [searchParams]);
 
+    const pagina = useMemo(() => {
+        return Number(searchParams.get('pagina')) || 1;
+    }, [searchParams]);
+
     useEffect(() => {
 
         setIsLoading(true);
 
         debounce(() => {
-            PessoasService.getAll(1, busca)
+            PessoasService.getAll(pagina, busca)
                 .then((result) => {
                     setIsLoading(false);
 
@@ -40,7 +44,7 @@ export const ListagemDePessoas: React.FC = () => {
                 });
         });
 
-    }, [busca]);
+    }, [busca, pagina]);
 
     return (
         <LayoutBaseDePagina
@@ -50,7 +54,11 @@ export const ListagemDePessoas: React.FC = () => {
                     mostrarInputBusca
                     textoBotaoNovo="Nova"
                     textoDaBusca={busca}
-                    aoMudarTextoDaBusca={texto => setSearchParams({ busca: texto }, { replace: true })}
+                    aoMudarTextoDaBusca={
+                        texto => setSearchParams(
+                            { busca: texto, pagina: '1' },
+                            { replace: true }
+                        )}
                 />
             }
         >
@@ -92,6 +100,16 @@ export const ListagemDePessoas: React.FC = () => {
                     </TableFooter>
                 </Table>
             </TableContainer>
+
+            {(totalCount > 0 && totalCount > Environment.LIMITE_DE_LINHAS) && (
+                <Box sx={{ display: 'flex', justifyContent: 'center', m: 1 }}>
+                    <Pagination
+                        page={pagina}
+                        count={Math.ceil(totalCount / Environment.LIMITE_DE_LINHAS)}
+                        onChange={(_, newPage) => setSearchParams({ busca, pagina: newPage.toString() }, { replace: true })}
+                    />
+                </Box>
+            )}
         </LayoutBaseDePagina>
     );
 }
